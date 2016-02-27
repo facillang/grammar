d6f13d4af09b3806eebcc2411228972f89ed17b5
blangel <langelb@gmail.com>
2016-02-27 08:21:09 -0500

    * Updating documentation. | * Adding ply configuration. | * Cleaning up style within Facil java files.

--- src/main/java/org/facillang/FacilTestRig.java
+++ src/main/java/org/facillang/FacilTestRig.java
@@ -1,11 +1,10 @@
 package org.facillang;
 
 import org.antlr.v4.gui.TestRig;
-/**
- * Created by sumanthdommaraju on 2/4/16.
- */
 
 /**
+ * Created by sumanthdommaraju on 2/4/16
+ *
  * This class will be useful to test grammar. (look for example arguments to use).
  */
 public class FacilTestRig extends TestRig{
@@ -15,21 +14,24 @@ public class FacilTestRig extends TestRig{
         this.inputFiles.add(inputFile);
     }
 
-    //mode fileName
-    //ex:-gui src/main/antlr4/Employee.facil
+    // mode fileName
+    // ex: -gui src/main/antlr4/Employee.facil
     public static void main(String[] args) throws Exception {
         validateArgs(args);
-        String[] argsForTestRig = new String[]{"org.facillang.Facil", "facilCompilation", args[0]};
+        String[] argsForTestRig = new String[] { "org.facillang.Facil", "facilCompilation", args[0] };
         FacilTestRig facilTestRig = new FacilTestRig(argsForTestRig, args[1]);
         facilTestRig.process();
     }
 
     private static void validateArgs(String[] args) {
-        if(args == null || args.length != 2 ||
-                (!args[0].equals("-tree") && !args[0].equals("-gui") )){
-            throw new RuntimeException("Invalid arguments. Usage: java org.facillang.FacilTestRig mode fileName");
+        if ((args == null) || (args.length != 2)
+                || (!"-tree".equals(args[0]) && !"-gui".equals(args[0]))) {
+            System.err.printf("Usage: java org.facillang.FacilTestRig mode filePath%n");
+            System.err.printf("    where 'mode' is either '-gui' or '-tree'%n");
+            System.err.printf("      and 'filePath' is a path to a facil file to parse%n");
+            throw new RuntimeException("Invalid arguments. Usage: java org.facillang.FacilTestRig mode filePath");
         }
-        if(!args[1].contains(".facil")){
+        if (!args[1].contains(".facil")) {
             throw new RuntimeException("Invalid file extension");
         }
     }
